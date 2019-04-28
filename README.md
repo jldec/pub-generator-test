6b20ef56b687222535f186b7ef590c96e5d8d852
Jurgen Leschner <jldec@ciaosoft.com>
2019-04-28 14:33:21 +0100

    lint

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![Azure Build Status](https://dev.azure.com/jldec/pub-test/_apis/build/status/jldec.pub-test?branchName=master)](https://dev.azure.com/jldec/pub-test/_build/latest?definitionId=1&branchName=master)
 
 This [pub-server](https://github.com/jldec/pub-server) test project
-tests pub-generator across different platforms without `.gitattributes` or `.editorconfig`. 
+tests pub-generator across different platforms without `.gitattributes` or EOL in `.editorconfig`.
 
 To validate that the text files all work even with different line endings.
 
--- test/test-pub.js
+++ test/test-pub.js
@@ -7,7 +7,7 @@ var fspath = require('path');
 function eol(){/*
 */}
 
-var EOL = eol.toString().slice(17,-3); 
+var EOL = eol.toString().slice(17,-3);
 
 console.log('EOL:', JSON.stringify(EOL));
 
@@ -27,71 +27,72 @@ test('test-pub', function(t) {
     cb && cb();
   };
 
-  var expected =
-  [ { path: '/index.html',
-  text: [
-    '<!DOCTYPE html>',
-    '<html lang="en">',
-    '<head>',
-    '<meta charset="utf-8">',
-    '<meta name="viewport" content="width=device-width, initial-scale=1">',
-    '',
-    '<link rel="stylesheet" href="/css/pub-test.css">',
-    '<title>/</title>',
-    '</head>',
-    '<body>',
-    '',
-    '<div data-render-layout="main-layout"><div data-render-page="default"><div data-render-html="/"><h1 id="hello-world">hello world</h1>',
-    '<p>go to <a href="/page1">page 1</a></p>',
-    '</div>',
-    '',
-    '</div>',
-    '</div>',
-    '',
-    '<script>window.pubRef = {"href":"/","relPath":""};</script>',
-    '',
-    '</body>',
-    '</html>',
-    ''
-  ].join(EOL) },
-{ path: '/page1.html',
-  text: [
-    '<!DOCTYPE html>',
-    '<html lang="en">',
-    '<head>',
-    '<meta charset="utf-8">',
-    '<meta name="viewport" content="width=device-width, initial-scale=1">',
-    '',
-    '<link rel="stylesheet" href="/css/pub-test.css">',
-    '<title>Page1</title>',
-    '</head>',
-    '<body>',
-    '',
-    '<div data-render-layout="main-layout"><div data-render-page="test-template"><div class="pub-test">',
-    '<div data-render-html="/page1"><h2 id="page-1">page 1</h2>',
-    '<p>go <a href="/">home</a></p>',
-    '</div>',
-    '</div>',
-    '',
-    '<div class="pub-test-fragment">',
-    '<div data-render-html="/page1#fragment-1"><h3 id="fragment-1">fragment 1</h3>',
-    '<p>text 1</p>',
-    '</div>',
-    '</div>',
-    '<div class="pub-test-fragment">',
-    '<div data-render-html="/page1#fragment-2"><h3 id="fragment-2">fragment 2</h3>',
-    '<p>text 2</p>',
-    '</div>',
-    '</div>',
-    '</div>',
-    '</div>',
-    '',
-    '<script>window.pubRef = {"href":"/page1","relPath":""};</script>',
-    '',
-    '</body>',
-    '</html>',
-    ''
-    ].join(EOL) } ];
+  var expected = [
+    { path: '/index.html',
+      text: [
+        '<!DOCTYPE html>',
+        '<html lang="en">',
+        '<head>',
+        '<meta charset="utf-8">',
+        '<meta name="viewport" content="width=device-width, initial-scale=1">',
+        '',
+        '<link rel="stylesheet" href="/css/pub-test.css">',
+        '<title>/</title>',
+        '</head>',
+        '<body>',
+        '',
+        '<div data-render-layout="main-layout"><div data-render-page="default"><div data-render-html="/"><h1 id="hello-world">hello world</h1>',
+        '<p>go to <a href="/page1">page 1</a></p>',
+        '</div>',
+        '',
+        '</div>',
+        '</div>',
+        '',
+        '<script>window.pubRef = {"href":"/","relPath":""};</script>',
+        '',
+        '</body>',
+        '</html>',
+        ''
+      ].join(EOL)
+    },
+    { path: '/page1.html',
+      text: [
+        '<!DOCTYPE html>',
+        '<html lang="en">',
+        '<head>',
+        '<meta charset="utf-8">',
+        '<meta name="viewport" content="width=device-width, initial-scale=1">',
+        '',
+        '<link rel="stylesheet" href="/css/pub-test.css">',
+        '<title>Page1</title>',
+        '</head>',
+        '<body>',
+        '',
+        '<div data-render-layout="main-layout"><div data-render-page="test-template"><div class="pub-test">',
+        '<div data-render-html="/page1"><h2 id="page-1">page 1</h2>',
+        '<p>go <a href="/">home</a></p>',
+        '</div>',
+        '</div>',
+        '',
+        '<div class="pub-test-fragment">',
+        '<div data-render-html="/page1#fragment-1"><h3 id="fragment-1">fragment 1</h3>',
+        '<p>text 1</p>',
+        '</div>',
+        '</div>',
+        '<div class="pub-test-fragment">',
+        '<div data-render-html="/page1#fragment-2"><h3 id="fragment-2">fragment 2</h3>',
+        '<p>text 2</p>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '</div>',
+        '',
+        '<script>window.pubRef = {"href":"/page1","relPath":""};</script>',
+        '',
+        '</body>',
+        '</html>',
+        ''
+      ].join(EOL) } ];
 
   generator.load(function(err) {
     t.error(err);
